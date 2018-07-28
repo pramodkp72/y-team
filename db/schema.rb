7380688ca973a9f1eba2ea0797720ed6240344a6
Eric Hosford <eric.hosford@gmail.com>
2018-07-27 23:38:14 -0400

    added Place Comments model.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_07_28_030359) do
+ActiveRecord::Schema.define(version: 2018_07_28_033453) do
 
   create_table "events", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name"
@@ -19,6 +19,26 @@ ActiveRecord::Schema.define(version: 2018_07_28_030359) do
     t.string "end_time"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "places_id"
+    t.integer "category"
+    t.bigint "user_id"
+    t.boolean "enabled"
+    t.index ["places_id"], name: "index_events_on_places_id"
+    t.index ["user_id"], name: "index_events_on_user_id"
+  end
+
+  create_table "place_comments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.integer "rating"
+    t.string "title"
+    t.text "text"
+    t.bigint "user_id"
+    t.bigint "place_id"
+    t.boolean "flagged"
+    t.boolean "enabled"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["place_id"], name: "index_place_comments_on_place_id"
+    t.index ["user_id"], name: "index_place_comments_on_user_id"
   end
 
   create_table "places", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
@@ -58,6 +78,10 @@ ActiveRecord::Schema.define(version: 2018_07_28_030359) do
     t.boolean "enabled"
   end
 
+  add_foreign_key "events", "places", column: "places_id"
+  add_foreign_key "events", "users"
+  add_foreign_key "place_comments", "places"
+  add_foreign_key "place_comments", "users"
   add_foreign_key "places", "users"
   add_foreign_key "profiles", "users"
 end
