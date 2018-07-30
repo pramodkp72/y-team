457d758b16c11bdad4cab6f3138660d25ccc2475
Eric Hosford <eric.hosford@gmail.com>
2018-07-30 10:56:53 -0400

    worked on events form

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,6 @@
 class ApplicationController < ActionController::Base
+  Rails.application.config.active_record.time_zone_aware_types = [:datetime, :time]
+  # config.active_record.time_zone_aware_types = [:datetime, :time]
 
   helper_method :current_user
 
